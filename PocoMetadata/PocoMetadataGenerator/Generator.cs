17073d99a9d017d5d03ca6ebca61e16b976ffff3
Alex Efimov <alexander.efimov@arcadia.spb.ru>
2018-01-24 18:15:44 +0300

    Added CLI parameter to support external entity descriptor. Added support for multiple input assemblies.

--- PocoMetadata/PocoMetadataGenerator/Generator.cs
+++ PocoMetadata/PocoMetadataGenerator/Generator.cs
@@ -9,29 +9,51 @@ namespace Breeze.PocoMetadata
     /// </summary>
     public class Generator
     {
-        /// <summary>
-        /// Load the assembly from the given file, then call <see cref="Generate(Assembly, EntityDescriptor)"/>
-        /// </summary>
-        /// <param name="assemblyFileName"></param>
-        /// <param name="descriptor"></param>
-        /// <returns></returns>
-        public static Metadata Generate(string assemblyFileName, EntityDescriptor descriptor)
+       /// <summary>
+       /// Load the assembly from the given file, then call <see cref="Generate(Assembly, EntityDescriptor)"/>
+       /// </summary>
+       /// <param name="assemblyFileName"></param>
+       /// <param name="descriptor"></param>
+       /// <returns></returns>
+       public static Metadata Generate(string assemblyFileName, EntityDescriptor descriptor)
+       {
+          Assembly assembly = Assembly.LoadFrom(assemblyFileName);
+          return Generate(new[] { assembly }, descriptor);
+       }
+
+       /// <summary>
+       /// Load the assembly from the given AssemblyName, then call <see cref="Generate(Assembly, EntityDescriptor)"/>
+       /// </summary>
+       /// <param name="assemblyName"></param>
+       /// <param name="descriptor"></param>
+       /// <returns></returns>
+       public static Metadata Generate(AssemblyName assemblyName, EntityDescriptor descriptor)
+       {
+          Assembly assembly = Assembly.Load(assemblyName.FullName);
+          return Generate(new [] { assembly }, descriptor);
+       }
+
+      /// <summary>
+      /// Load the assembly from the given files, then call <see cref="Generate(Assembly, EntityDescriptor)"/>
+      /// </summary>
+      /// <param name="assemblyFileNames"></param>
+      /// <param name="descriptor"></param>
+      /// <returns></returns>
+      public static Metadata Generate(string[] assemblyFileNames, EntityDescriptor descriptor)
         {
-            //Assembly assembly = Assembly.ReflectionOnlyLoadFrom(assemblyFileName);
-            Assembly assembly = Assembly.LoadFrom(assemblyFileName);
+            Assembly[] assembly = assemblyFileNames.Select(Assembly.LoadFrom).ToArray();
             return Generate(assembly, descriptor);
         }
 
         /// <summary>
-        /// Load the assembly from the given AssemblyName, then call <see cref="Generate(Assembly, EntityDescriptor)"/>
+        /// Load the assembly from the given AssemblyNamse, then call <see cref="Generate(Assembly, EntityDescriptor)"/>
         /// </summary>
-        /// <param name="assemblyName"></param>
+        /// <param name="assemblyNames"></param>
         /// <param name="descriptor"></param>
         /// <returns></returns>
-        public static Metadata Generate(AssemblyName assemblyName, EntityDescriptor descriptor)
+        public static Metadata Generate(AssemblyName[] assemblyNames, EntityDescriptor descriptor)
         {
-            //Assembly assembly = Assembly.ReflectionOnlyLoad(assemblyName.FullName);
-            Assembly assembly = Assembly.Load(assemblyName.FullName);
+            Assembly[] assembly = assemblyNames.Select(n => Assembly.Load(n.FullName)).ToArray();
             return Generate(assembly, descriptor);
         }
 
@@ -41,17 +63,17 @@ namespace Breeze.PocoMetadata
         /// <param name="assembly">Assembly containing entity types</param>
         /// <param name="descriptor">Descriptor used to evaluate types and resolve metadata</param>
         /// <returns></returns>
-        public static Metadata Generate(Assembly assembly, EntityDescriptor descriptor)
+        public static Metadata Generate(Assembly[] assembly, EntityDescriptor descriptor)
         {
             var builder = new PocoMetadataBuilder(descriptor);
-            var types = GetTypesFromAssembly(assembly);
+            var types = GetTypesFromAssemblies(assembly);
             var metadata = builder.BuildMetadata(types);
             return metadata;
         }
 
-        private static Type[] GetTypesFromAssembly(Assembly assembly)
+        private static Type[] GetTypesFromAssemblies(Assembly[] assembly)
         {
-            var types = assembly.GetExportedTypes();
+            var types = assembly.SelectMany(s => s.GetExportedTypes());
 
             return types.ToArray();
         }
